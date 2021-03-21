a1049e9f4b39cd92e28d2e34dc824b868c88c30a
SaptakSTree <sadcat14@gmail.com>
2021-03-21 11:19:03 +0530

    no message

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+import cv2 # pip install opencv-python
+import PIL.Image, PIL.ImageTk # pip install pillow
+
+SET_WIDTH = 650
+SET_HEIGHT = 368
+
+window = tkinter.Tk()
+window.title("Third Umpire DRS kit")
+
+print("hii")
